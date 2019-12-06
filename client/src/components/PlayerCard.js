c3b90ce4a0e107af5e5b9d08b6959cbf6288b89c
trodriguez89 <trodriguez89@gmail.com>
2019-12-06 08:54:13 -0800

    mvp wip

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -1,17 +1,34 @@
 import React from "react";
+import styled from"styled-components";
+
+const PlayerContain = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+`
+const PlayerDiv = styled.div`
+    border: 2px solid black;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 250px;
+    height: 250px;
+    margin-top: 10px;
+`
 
 const PlayerCard = props => {
     return (
-        <div>
+        <PlayerContain>
             {props.info.map(item => (
-                <div>
+                <PlayerDiv>
                     <h2>Name: {item.name}</h2>
                     <h3>Country: {item.country}</h3>
                     <h4>Searches: {item.searches}</h4>
-                </div>
+                </PlayerDiv>
             ))}
 
-        </div>
+        </PlayerContain>
     );
 }
 
